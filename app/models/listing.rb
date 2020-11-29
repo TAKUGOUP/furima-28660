9f13330be95d1cf37b768635570bc2a57df63c63
TAKUGOUP <hirofusa.takuei@gmail.com>
2020-11-29 15:48:02 +0900

    謎のモデル『listing』の削除（migrateファイルも）

--- app/models/listing.rb
+++
@@ -1,9 +0,0 @@
-class Listing < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to : category, status, delivery_fee, prefecture, days_to_ship 
-
-  validates :name, :content, presence: true
-
-  validates :category_id, :status_id, :delivery_fee_id, :prefecture_id, :days_to_ship_id, numericality: { other_than: 1 } 
-
-end
